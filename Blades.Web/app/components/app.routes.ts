fb2b312484b8ced3629a8946e33e9a9382e46264
durmisi <durmisi@gmail.com>
2017-06-09 21:51:41 +0200

    add spa fallback route

--- Blades.Web/app/components/app.routes.ts
+++ Blades.Web/app/components/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', loadChildren: './home/home.module#HomeModule' },
     { path: 'designer', loadChildren: './designer/designer.module#DesignerModule' },
     { path: 'blade', loadChildren: './blade/blade.module#BladeModule' },
 ];
